7acd4d5236be54fbaf4e887f3f22e967e31e15bc
Mike Potanin <potanin@gmail.com>
2014-03-27 19:20:13 +0400

    Fix "Let" cjmparation.

--- src/main/org/nlogo/nvm/ContextHelper.scala
+++ src/main/org/nlogo/nvm/ContextHelper.scala
@@ -5,9 +5,9 @@ import org.nlogo.api.Let
 
 object ContextHelper {
   def getLet(j:Job, bindigs:List[LetBinding], l:Let):java.lang.Object = {
-    bindigs.find(l==_.let).map(_.value).getOrElse(j.parentContext.getLet(l))
+    bindigs.find(l eq _.let).map(_.value).getOrElse(j.parentContext.getLet(l))
   }
   def setLet(j:Job, bindigs:List[LetBinding], l:Let, v:java.lang.Object) {
-   bindigs.find(l==_.let).map(_.value = v).getOrElse(j.parentContext.setLet(l,v))
+   bindigs.find(l eq _.let).map(_.value = v).getOrElse(j.parentContext.setLet(l,v))
   }
 }
